80bcb809c84e891290f6d321061631df067303d1
asimory <151659237+asimory@users.noreply.github.com>
2023-11-23 23:48:26 +0530

    Update asimory.js

--- asimory.js
+++ asimory.js
@@ -11,7 +11,7 @@ let buttons = document.querySelectorAll('.majbut');
     
 
     Array.from(buttons).forEach((buttons) => {
-      buttons.addEventListener('touchstart', (e) => {
+      buttons.addEventListener('touchend', (e) => {
           string = string + e.target.innerHTML;
           document.querySelector("textarea").value = string;
         })})
