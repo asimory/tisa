bcc71155d46ed9b4fc8c9a02ec69e2f04d7198a6
asimory <151659237+asimory@users.noreply.github.com>
2023-11-24 00:03:49 +0530

    Update asimory.js

--- asimory.js
+++ asimory.js
@@ -5,6 +5,11 @@ let buttons = document.querySelectorAll('.majbut');
 
 
 
+  Array.from(buttons).forEach((buttons) => {
+  buttons.addEventListener('click', (e) => {
+      string = string + e.target.innerHTML;
+      document.querySelector("textarea").value = string;
+    })})
 
     
 
